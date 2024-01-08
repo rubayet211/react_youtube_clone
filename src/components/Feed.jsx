8a937b2292fcdf0bdaaacd6b5960c1052a7301b6
Rhyme Rubayet <rubayet211@gmail.com>
2024-01-09 00:33:01 +0600

    Sidebar

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,43 @@
+import { Box, Stack, Typography } from "@mui/material";
 import React from "react";
+import { Sidebar, Videos } from "../components";
 
 const Feed = () => {
   return (
     <div>
-      <h1>Feed</h1>
+      <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+        <Box
+          sx={{
+            height: { sx: "auto", md: "92vh" },
+            borderRadius: "1px solid #3d3d3d",
+            px: { sx: 0, md: 2 },
+          }}
+        >
+          <Sidebar />
+          <Typography
+            className="copyright"
+            variant="body2"
+            sx={{ mt: 1.5, color: "#fff" }}
+          >
+            Copyright 2024 Rhythm Media
+          </Typography>
+        </Box>
+
+        <Box p={2} sx={{ overflowY: "auto ", height: "90vh", flex: 2 }}>
+          <Typography
+            variant="h4"
+            fontWeight="bold"
+            mb={2}
+            sx={{
+              color: "white",
+            }}
+          >
+            New
+            <span style={{ color: "#FC1503" }}> videos</span>
+          </Typography>
+          <Videos />
+        </Box>
+      </Stack>
     </div>
   );
 };
